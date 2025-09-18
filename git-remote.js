732cd4c223cede8e82e4be4e106b5e0cee31115b
M Hasan <hasanmuhammad197@gmail.com>
2025-09-18 18:56:53 +0700

    Git Remote

---
+++ git-remote.js
@@ -0,0 +1,6 @@
+//Git Remote
+//Untuk dapat kolaborasi dengan team, repository git perlu ditaruh di server bukan di lokal
+//Git mendukung centralized server yg membuat bisa menyimpan perubahan git repository di komputer ke server
+//Serta merupakan distributed version control, kita tidak wajib terkoneksi inet ketika mengelola repository
+//Inet hanya dibutuhkan ketika mengirim perubahan/mengambil perubahan ke/dari server
+//Git juga dapat mendukung penggunaan beberapa git server sekaligus ketika membuat git repository
